4aa523703f8f95834d769ad7723f3432656d0117
Pritam Dhaugoda <pritamdhaugoda@gmail.com>
2024-09-28 13:33:57 +0545

    Fix error

--- src/app/page.js
+++ src/app/page.js
@@ -4,7 +4,7 @@ const API_KEY = process.env.API_KEY;
 
 export default async function Home({ searchParams }) {
   const genre = searchParams.genre || "trending/all/day";
-  console.log(gen)
+  
   const res = await fetch(
     `https://api.themoviedb.org/3/${genre}?api_key=${API_KEY}&language=en-US&page=1`,
     { next: { revalidate: 1000 } }
